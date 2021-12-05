44c1826706fc1b47e1d707c39c1c3388914ad150
Byron Hallett <byron.james.h@gmail.com>
2021-12-06 09:32:51 +1100

    add emoji diff tracking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net/http"
 	"os"
@@ -13,6 +14,7 @@ import (
 )
 
 const emoji_url = "https://emojipedia.org"
+const emoji_file = "emoji_data"
 
 var client *gateway.Session
 
@@ -51,12 +53,54 @@ func main() {
 	})
 	emojis := pops.ChildrenFiltered("ul").ChildrenFiltered("li")
 
+	// load the previous ranks
+	prev_ranks := make(map[string]int)
+	// try to fetch old store
+	file, emoji_err := os.Open(emoji_file)
+	if emoji_err != nil {
+		log.Println("Save file not found, creating.")
+	} else {
+		scan := bufio.NewScanner(file)
+		rank := 0
+		for scan.Scan() {
+			prev_ranks[scan.Text()] = rank
+			rank += 1
+		}
+	}
+
 	// construct message
 	var message string
+	var emoji_string string
 	emojis.Each(func(i int, s *goquery.Selection) {
-		message += strconv.Itoa(i+1) + ": " + s.Text() + "\n"
+		current_emoji := s.Text()
+
+		// modify rank before aappending
+		prev, present := prev_ranks[current_emoji]
+		if !present {
+			prev = emojis.Size()
+		}
+		shift := prev - i
+
+		emoji_string += current_emoji + "\n"
+		shift_string := strconv.Itoa(shift)
+		if shift > 0 {
+			shift_string = "↑" + shift_string
+		}
+		if shift < 0 {
+			shift_string = "↓" + shift_string[1:]
+		}
+		message += strconv.Itoa(i+1) + ": " + current_emoji + ", shift: " + shift_string + "\n"
 	})
 	message = strings.TrimSpace(message)
+
+	// store emojis for comparison
+	file, err = os.Create(emoji_file)
+	if err != nil {
+		_, err = client.Channel.Send(channel, "Can't save emoji data!")
+
+		log.Fatalln(err.Error())
+	}
+	file.Write([]byte(emoji_string))
 
 	// send to discord
 	_, err = client.Channel.Send(channel, message)
